7eb321f9f16282243344044a106cd04727aef66f
isam chajia <isamchajiabeat.com>
2025-02-10 09:33:12 +0100

    set up the project

--- project/public/index.php
+++ project/public/index.php
@@ -15,11 +15,13 @@
   $dotenv = Dotenv::createImmutable(dirname(__DIR__));
   $dotenv->load();
   
+
+  echo 'hh';
   
   echo $_ENV['APP_NAME'];
   
 
-  dump(dirname(__DIR__).'/');
+  // dump(dirname(__DIR__).'/');
 
 
 
